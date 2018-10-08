d72d594686b2ce3060125754e23a0f0eea52aeac
Your Name <you@example.c>
2018-10-08 18:25:01 -0400

    production buildd docs created

---
+++ docs/precache-manifest.f611230d65a84ff01b2194a9e1740718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "31a919b0bf7c20c5c08d193d00938f14",
+    "url": "/Clicky-Pusheen/static/media/pink_dots.31a919b0.svg"
+  },
+  {
+    "revision": "2cc5c41952d35aa40395",
+    "url": "/Clicky-Pusheen/static/js/runtime~main.2cc5c419.js"
+  },
+  {
+    "revision": "6dbb2e6ccfa9b1536ed6",
+    "url": "/Clicky-Pusheen/static/js/main.6dbb2e6c.chunk.js"
+  },
+  {
+    "revision": "115b106e8a6c3b9b3995",
+    "url": "/Clicky-Pusheen/static/js/1.115b106e.chunk.js"
+  },
+  {
+    "revision": "6dbb2e6ccfa9b1536ed6",
+    "url": "/Clicky-Pusheen/static/css/main.8c4fe915.chunk.css"
+  },
+  {
+    "revision": "4a3dbf93daeb3ffde349c19bca98c919",
+    "url": "/Clicky-Pusheen/index.html"
+  }
+];
